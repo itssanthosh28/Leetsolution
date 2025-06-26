eed94b2de06fa5b4fa7dab99e607259674bcad41
itssanthosh28 <marksanthosh2004@gmail.com>
2025-06-26 15:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (52.64%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isHappy(int n) {
+    //     ArrayList<Integer> list =new ArrayList<>();
+    //     n=digitsquare(n);
+    //     while(n!=1){
+    //         if(list.contains(n)){
+    //             return false;
+    //         }
+    //             list.add(n);                     
+    //     }
+    //    return true;
+        
+    // }
+    int slow=n;
+    int fast=n;
+    do{
+    slow=digitsquare(slow);
+    fast=digitsquare(digitsquare(fast));
+    }while(slow!=fast);
+    if(slow==1)return true;
+    return false;
+    }
+
+    
+
+    public static int digitsquare(int n){
+        int squaresum=0;
+        while(n>0){
+            int a=n%10;
+           // squaresum+=Math.pow(a,2);
+           squaresum+=a*a;
+            n=n/10;
+        }
+   return squaresum;
+    }
+}
