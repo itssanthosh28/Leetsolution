432a3495699da85d447e7a39730a930e195273f8
itssanthosh28 <marksanthosh2004@gmail.com>
2025-06-26 13:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (51.95%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,33 +11,49 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode fast=head;
-        ListNode slow =head;
-        ListNode fast1=head;
-        ListNode slow1=head;
-        int count =0;
+        // ListNode fast=head;
+        // ListNode slow =head;
+        // ListNode fast1=head;
+        // ListNode slow1=head;
+        // int count =0;
     
+        // while(fast!=null && fast.next!=null){
+        //     slow=slow.next;
+        //     fast=fast.next.next;
+        //     if(fast==slow){
+        //         ListNode temp=slow;
+        //         do{
+        //             temp=temp.next;
+        //             count++;
+        //             } while(temp!=slow);
+              
+        //       while(count-->0){
+        //         slow1=slow1.next;
+        //       }
+        //       while(slow1!=fast1){
+        //         slow1=slow1.next;
+        //         fast1=fast1.next;
+        //       }
+                        
+        //   return fast1;  
+        //     }
+        // }
+        // return null;   
+
+        ListNode slow=head;
+        ListNode fast=head;
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            if(fast==slow){
-                ListNode temp=slow;
-                do{
-                    temp=temp.next;
-                    count++;
-                    } while(temp!=slow);
-              
-              while(count-->0){
-                slow1=slow1.next;
-              }
-              while(slow1!=fast1){
-                slow1=slow1.next;
-                fast1=fast1.next;
-              }
-                        
-          return fast1;  
+            if(slow==fast){
+                ListNode entry=head;
+                while(entry!=slow){
+                    slow=slow.next;
+                    entry=entry.next;
+                }
+                return slow;
             }
         }
-        return null;     
+        return null;  
     }
 }
